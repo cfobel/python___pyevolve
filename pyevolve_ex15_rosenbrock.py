23ebc2cfa60d6ec70dc28659250342ba2da30926
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-03-04 19:54:50 +0000

    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@122 07ed1b73-6f11-4a40-98be-48c2d1321411

--- pyevolve_ex15_rosenbrock.py
+++ pyevolve_ex15_rosenbrock.py
@@ -3,39 +3,43 @@ from pyevolve import Initializators, Mutators, Consts, DBAdapters
 
 # This is the Rosenbrock Function, a deception function
 def rosenbrock(xlist):
-   sum1 = 0
+   sum_var = 0
    for x in xrange(1, len(xlist)):
-      sum1 += 100.0 * (xlist[x] - xlist[x-1]**2)**2 + (1 - xlist[x-1])**2
+      sum_var += 100.0 * (xlist[x] - xlist[x-1]**2)**2 + (1 - xlist[x-1])**2
+   return sum_var
 
-   return sum1
+if __name__ == "__main__":
 
-# Genome instance
-genome = G1DList.G1DList(20)
-genome.setParams(rangemin=0, rangemax=10, bestRawScore=0.0)
-genome.mutator.set(Mutators.G1DListMutatorIntegerRange)
+   # Genome instance
+   genome = G1DList.G1DList(10)
+   genome.setParams(rangemin=-5, rangemax=10, bestRawScore=0.0)
+   genome.mutator.set(Mutators.G1DListMutatorIntegerRange)
 
-# The evaluator function (objective function)
-genome.evaluator.set(rosenbrock)
+   # The evaluator function (objective function)
+   genome.evaluator.set(rosenbrock)
 
-# Genetic Algorithm Instance
-ga = GSimpleGA.GSimpleGA(genome)
-ga.minimax = Consts.minimaxType["minimize"]
-ga.setGenerations(4000)
-ga.setMutationRate(0.2)
-ga.terminationCriteria.set(GSimpleGA.RawScoreCriteria)
+   # Genetic Algorithm Instance
+   ga = GSimpleGA.GSimpleGA(genome)
+   ga.setMinimax(Consts.minimaxType["minimize"])
+   #ga.selector.set(Selectors.GRouletteWheel)
+   ga.setGenerations(5000)
+   ga.setCrossoverRate(0.6)
+   ga.setPopulationSize(400)
+   ga.setMutationRate(0.08)
+   ga.terminationCriteria.set(GSimpleGA.RawScoreCriteria)
 
-# Create DB Adapter and set as adapter
-sqlite_adapter = DBAdapters.DBSQLite(identify="rosenbrock")
-ga.setDBAdapter(sqlite_adapter)
+   # Create DB Adapter and set as adapter
+   #sqlite_adapter = DBAdapters.DBSQLite(identify="rosenbrock")
+   #ga.setDBAdapter(sqlite_adapter)
 
-# Do the evolution, with stats dump
-# frequency of 10 generations
-ga.evolve(freq_stats=200)
+   # Do the evolution, with stats dump
+   # frequency of 10 generations
+   ga.evolve(freq_stats=50)
 
-# Best individual
-best = ga.bestIndividual()
-print "\nBest individual score: %.2f" % (best.score,)
-print best
+   # Best individual
+   best = ga.bestIndividual()
+   print "\nBest individual score: %.2f" % (best.score,)
+   print best
 
 
 
