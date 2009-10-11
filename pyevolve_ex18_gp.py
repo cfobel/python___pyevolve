56e992dda6b55508351af75102979ab34062257e
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-10-11 23:16:24 +0000

    Used in benchmark for multiprocessing
    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@230 07ed1b73-6f11-4a40-98be-48c2d1321411

--- pyevolve_ex18_gp.py
+++ pyevolve_ex18_gp.py
@@ -1,24 +1,18 @@
-from pyevolve import GSimpleGA
-from pyevolve import GTree
-from pyevolve import Consts
-from pyevolve import Selectors
-from pyevolve import Mutators
-from pyevolve import Util
+from pyevolve import *
 import math
-import random
 
-rmse_accum = Util.RMSEAccumulator()
+rmse_accum = Util.ErrorAccumulator()
 
-@GTree.gpdec(representation="+", color="red")
+#@GTree.gpdec(representation="+", color="red")
 def gp_add(a, b): return a+b
 
-@GTree.gpdec(representation="-")
+#@GTree.gpdec(representation="-")
 def gp_sub(a, b): return a-b
 
-@GTree.gpdec(representation="*")
+#@GTree.gpdec(representation="*")
 def gp_mul(a, b): return a*b
 
-@GTree.gpdec(representation="sqrt")
+#@GTree.gpdec(representation="sqrt")
 def gp_sqrt(a):   return math.sqrt(abs(a))
    
 def eval_func(chromosome):
@@ -26,41 +20,40 @@ def eval_func(chromosome):
    rmse_accum.reset()
    code_comp = chromosome.getCompiledCode()
    
-   for a in xrange(0, 5):
-      for b in xrange(0, 5):
+   for a in xrange(0, 80):
+      for b in xrange(0, 80):
          evaluated     = eval(code_comp)
          target        = math.sqrt((a*a)+(b*b))
-         rmse_accum += (target, evaluated)
+         rmse_accum   += (target, evaluated)
    return rmse_accum.getRMSE()
 
 def callback_draw(ga_engine):
-   GTree.writeGTreeGPPopulation(ga_engine, "full.jpg")
+   if ga_engine.getCurrentGeneration() == 0:
+      GTree.GTreeGP.writePopulation(ga_engine, "full.jpg", 0, 10)
    return False
 
 
 def main_run():
    genome = GTree.GTreeGP()
-   root   = GTree.GTreeNodeGP('a', Consts.nodeType["TERMINAL"])
-   genome.setRoot(root)
-
    genome.setParams(max_depth=4, method="ramped")
    genome.evaluator += eval_func
    genome.mutator.set(Mutators.GTreeGPMutatorSubtree)
 
-   ga = GSimpleGA.GSimpleGA(genome)
+   ga = GSimpleGA.GSimpleGA(genome, seed=666)
    ga.setParams(gp_terminals       = ['a', 'b'],
                 gp_function_prefix = "gp")
-   ga.selector.set(Selectors.GRouletteWheel)
 
    ga.setMinimax(Consts.minimaxType["minimize"])
-   ga.setGenerations(1)
-   ga.stepCallback.set(callback_draw)
+   ga.setGenerations(20)
+   #ga.stepCallback.set(callback_draw)
    ga.setCrossoverRate(1.0)
    ga.setMutationRate(0.08)
-   ga.setPopulationSize(10)
+   ga.setPopulationSize(800)
+   ga.setMultiProcessing(False)
    
-   ga(freq_stats=10)
-   #print ga.bestIndividual()
+   ga(freq_stats=5)
+   best = ga.bestIndividual()
+   #print best
 
 if __name__ == "__main__":
    main_run()
