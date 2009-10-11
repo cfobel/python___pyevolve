6763187952dfda3bb8d336f1529ba90a1e4fe9d8
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-10-11 23:27:48 +0000

    Removed comments
    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@231 07ed1b73-6f11-4a40-98be-48c2d1321411

--- pyevolve_ex18_gp.py
+++ pyevolve_ex18_gp.py
@@ -3,16 +3,9 @@ import math
 
 rmse_accum = Util.ErrorAccumulator()
 
-#@GTree.gpdec(representation="+", color="red")
 def gp_add(a, b): return a+b
-
-#@GTree.gpdec(representation="-")
 def gp_sub(a, b): return a-b
-
-#@GTree.gpdec(representation="*")
 def gp_mul(a, b): return a*b
-
-#@GTree.gpdec(representation="sqrt")
 def gp_sqrt(a):   return math.sqrt(abs(a))
    
 def eval_func(chromosome):
@@ -27,12 +20,6 @@ def eval_func(chromosome):
          rmse_accum   += (target, evaluated)
    return rmse_accum.getRMSE()
 
-def callback_draw(ga_engine):
-   if ga_engine.getCurrentGeneration() == 0:
-      GTree.GTreeGP.writePopulation(ga_engine, "full.jpg", 0, 10)
-   return False
-
-
 def main_run():
    genome = GTree.GTreeGP()
    genome.setParams(max_depth=4, method="ramped")
@@ -45,7 +32,6 @@ def main_run():
 
    ga.setMinimax(Consts.minimaxType["minimize"])
    ga.setGenerations(20)
-   #ga.stepCallback.set(callback_draw)
    ga.setCrossoverRate(1.0)
    ga.setMutationRate(0.08)
    ga.setPopulationSize(800)
@@ -53,7 +39,6 @@ def main_run():
    
    ga(freq_stats=5)
    best = ga.bestIndividual()
-   #print best
 
 if __name__ == "__main__":
    main_run()
