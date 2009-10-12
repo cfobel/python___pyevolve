e3501df48263ffddb02c82678855996e1bf2cb0d
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-10-12 20:06:56 +0000

    Sphinx migration for Python 2.6
    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@233 07ed1b73-6f11-4a40-98be-48c2d1321411

--- build_docs_rel_0_6.bat
+++ build_docs_rel_0_6.bat
@@ -1,2 +1,3 @@
 @echo off
-c:\python25\scripts\sphinx-build -E -a -b html .\docs\source_rel_0_6 .\docs\build_rel_0_6
+rem c:\python25\scripts\sphinx-build -E -a -b html .\docs\source_rel_0_6 .\docs\build_rel_0_6
+c:\python26\scripts\sphinx-build -E -a -b html .\docs\source_rel_0_6 .\docs\build_rel_0_6
