5aab87f257a959e5266fd1ef68a97dbe8591c5ea
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-10-18 21:47:34 +0000

    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@245 07ed1b73-6f11-4a40-98be-48c2d1321411

--- pyevolve_ex13_sphere.py
+++ pyevolve_ex13_sphere.py
@@ -3,21 +3,21 @@ import math
 
 # This is the Sphere Function
 def sphere(xlist):
-   n = len(xlist)
+   #n = len(xlist)
+   #total = 0
+   #for i in xrange(n):
+   #   total += (xlist[i]**2)
    total = 0
-   for i in range(n):
-      total += (xlist[i]**2)
+   for i in xlist:
+      total += i**2
+
    return total
 
 
-if __name__ == "__main__":
-   
-   #import psyco
-   #psyco.full()
-
+def run_main():
    # Genome instance
-   genome = G1DList.G1DList(50)
-   genome.setParams(rangemin=-5.12, rangemax=5.13, bestrawscore=0.00, rounddecimal=2)
+   genome = G1DList.G1DList(140)
+   genome.setParams(rangemin=-5.12, rangemax=5.13)
    genome.initializator.set(Initializators.G1DListInitializatorReal)
    genome.mutator.set(Mutators.G1DListMutatorRealGaussian)
 
@@ -25,22 +25,19 @@ if __name__ == "__main__":
    genome.evaluator.set(sphere)
 
    # Genetic Algorithm Instance
-   ga = GSimpleGA.GSimpleGA(genome)
+   ga = GSimpleGA.GSimpleGA(genome, seed=666)
    ga.setMinimax(Consts.minimaxType["minimize"])
    ga.setGenerations(1500)
-   ga.setMutationRate(0.02)
-   ga.terminationCriteria.set(GSimpleGA.RawScoreCriteria)
-
-   # Create DB Adapter and set as adapter
-   # sqlite_adapter = DBAdapters.DBSQLite(identify="sphere")
-   # ga.setDBAdapter(sqlite_adapter)
+   ga.setMutationRate(0.01)
 
    # Do the evolution, with stats dump
    # frequency of 10 generations
-   ga.evolve(freq_stats=40)
+   ga.evolve(freq_stats=500)
 
    # Best individual
    best = ga.bestIndividual()
-   print "\nBest individual score: %.2f" % (best.score,)
-   print best
+
+if __name__ == "__main__":
+   run_main()
+   
 
