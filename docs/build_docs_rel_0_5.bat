ee8783851aaf11e946af515a7736bad78ce20c87
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-10-26 01:45:28 +0000

    Moved doc scripts
    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@248 07ed1b73-6f11-4a40-98be-48c2d1321411

---
+++ docs/build_docs_rel_0_5.bat
@@ -0,0 +1,3 @@
+@echo off
+c:\python25\scripts\sphinx-build -E -a -b html .\docs\source_rel_0_5 .\docs\build_rel_0_5
+rem c:\python25\scripts\sphinx-build -E -a -b latex .\docs\source_rel_0_5 .\docs\latex_rel_0_5
---
+++ docs/build_docs_rel_0_6.bat
@@ -0,0 +1,3 @@
+@echo off
+rem c:\python25\scripts\sphinx-build -E -a -b html .\docs\source_rel_0_6 .\docs\build_rel_0_6
+c:\python26\scripts\sphinx-build -E -a -b html .\docs\source_rel_0_6 .\docs\build_rel_0_6
