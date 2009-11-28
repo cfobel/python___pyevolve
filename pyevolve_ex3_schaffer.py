7070943d517b581d9a86225846edd4f7f31388a0
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-11-28 01:46:05 +0000

    Fixed the code for the Schaffer F6 function
    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@265 07ed1b73-6f11-4a40-98be-48c2d1321411

--- pyevolve_ex3_schaffer.py
+++ pyevolve_ex3_schaffer.py
@@ -2,21 +2,21 @@ from pyevolve import G1DList, GSimpleGA, Selectors
 from pyevolve import Initializators, Mutators, Consts
 import math
 
-# This is the Schaffer F6 Function, a deceptive function
-def schafferF6(xlist):
-   t1 = math.sin(math.sqrt(xlist[0]*xlist[0] + xlist[1]*xlist[1]));
-   t2 = 1 + 0.001*(xlist[0]*xlist[0] + xlist[1]*xlist[1]);
+# This is the Schaffer F6 Function
+# This function has been conceived by Schaffer, it's a 
+# multimodal function and it's hard for GAs due to the
+# large number of local minima, the global minimum is
+# at x=0,y=0 and there are many local minima around it
+def schafferF6(genome):
+   t1 = math.sin(math.sqrt(genome[0]**2 + genome[1]**2));
+   t2 = 1.0 + 0.001*(genome[0]**2 + genome[1]**2);
    score = 0.5 + (t1*t1 - 0.5)/(t2*t2)
    return score
 
-if __name__ == "__main__":
-
-   import psyco
-   psyco.full()      
-
+def run_main():
    # Genome instance
    genome = G1DList.G1DList(2)
-   genome.setParams(rangemin=-100, rangemax=100, bestRawScore=0.00, roundDecimal=2)
+   genome.setParams(rangemin=-100.0, rangemax=100.0, bestrawscore=0.0000, rounddecimal=4)
    genome.initializator.set(Initializators.G1DListInitializatorReal)
    genome.mutator.set(Mutators.G1DListMutatorRealGaussian)
 
@@ -27,16 +27,20 @@ if __name__ == "__main__":
    ga = GSimpleGA.GSimpleGA(genome)
    ga.selector.set(Selectors.GRouletteWheel)
 
-   ga.minimax = Consts.minimaxType["minimize"]
-   ga.setGenerations(5000)
+   ga.setMinimax(Consts.minimaxType["minimize"])
+   ga.setGenerations(8000)
    ga.setMutationRate(0.05)
+   ga.setPopulationSize(100)
    ga.terminationCriteria.set(GSimpleGA.RawScoreCriteria)
 
    # Do the evolution, with stats dump
    # frequency of 10 generations
-   ga.evolve(freq_stats=100)
+   ga.evolve(freq_stats=250)
 
    # Best individual
    best = ga.bestIndividual()
-   print "\nBest individual score: %.2f" % (best.score,)
-   #print best
+   print best
+   print "Best individual score: %.2f" % best.getRawScore()
+
+if __name__ == "__main__":
+   run_main()
