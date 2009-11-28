5f7612fc7a36f507b0e5bc1e8fdbc010a55b2eff
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-11-28 01:52:08 +0000

    Fixed code for the Ex4
    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@266 07ed1b73-6f11-4a40-98be-48c2d1321411

--- pyevolve_ex4_sigmatrunc.py
+++ pyevolve_ex4_sigmatrunc.py
@@ -1,4 +1,3 @@
-# $Id: pyevolve_ex4_sigmatrunc.py 150 2009-01-18 19:29:13Z christian.perone $
 from pyevolve import G1DList
 from pyevolve import GSimpleGA
 from pyevolve import Selectors
@@ -13,35 +12,38 @@ def eval_func(ind):
    var_z = var_x**2+2*var_x+1*math.cos(var_x)
    return var_z
 
-# Genome instance
-genome = G1DList.G1DList(1)
-genome.setParams(rangemin=-60.0, rangemax=60.0)
+def run_main():
+   # Genome instance
+   genome = G1DList.G1DList(1)
+   genome.setParams(rangemin=-60.0, rangemax=60.0)
 
-# Change the initializator to Real values
-genome.initializator.set(Initializators.G1DListInitializatorReal)
+   # Change the initializator to Real values
+   genome.initializator.set(Initializators.G1DListInitializatorReal)
 
-# Change the mutator to Gaussian Mutator
-genome.mutator.set(Mutators.G1DListMutatorRealGaussian)
+   # Change the mutator to Gaussian Mutator
+   genome.mutator.set(Mutators.G1DListMutatorRealGaussian)
 
-# Removes the default crossover
-genome.crossover.clear()
+   # Removes the default crossover
+   genome.crossover.clear()
 
-# The evaluator function (objective function)
-genome.evaluator.set(eval_func)
+   # The evaluator function (objective function)
+   genome.evaluator.set(eval_func)
 
-# Genetic Algorithm Instance
-ga = GSimpleGA.GSimpleGA(genome)
-ga.setMinimax(Consts.minimaxType["minimize"])
+   # Genetic Algorithm Instance
+   ga = GSimpleGA.GSimpleGA(genome)
+   ga.setMinimax(Consts.minimaxType["minimize"])
 
-pop = ga.getPopulation()
-pop.scaleMethod.set(Scaling.SigmaTruncScaling)
+   pop = ga.getPopulation()
+   pop.scaleMethod.set(Scaling.SigmaTruncScaling)
 
-ga.selector.set(Selectors.GRouletteWheel)
-ga.setGenerations(100)
+   ga.selector.set(Selectors.GRouletteWheel)
+   ga.setGenerations(100)
 
-# Do the evolution
-ga.evolve(10)
+   # Do the evolution
+   ga.evolve(10)
 
-# Best individual
-print ga.bestIndividual()
+   # Best individual
+   print ga.bestIndividual()
 
+if __name__ == "__main__":
+   run_main()
