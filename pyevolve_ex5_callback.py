b3efc7ca0b95a70d0c3f956af1e75d82e8cc0cb5
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-11-28 02:03:13 +0000

    Fixed code of the Ex5
    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@271 07ed1b73-6f11-4a40-98be-48c2d1321411

--- pyevolve_ex5_callback.py
+++ pyevolve_ex5_callback.py
@@ -1,8 +1,6 @@
-# $Id: pyevolve_ex5_callback.py 151 2009-01-19 01:23:22Z christian.perone $
 from pyevolve import G1DList
 from pyevolve import GSimpleGA
 from pyevolve import Selectors
-import pyevolve
 
 # The step callback function, this function
 # will be called every step (generation) of the GA evolution
@@ -15,34 +13,33 @@ def evolve_callback(ga_engine):
 
 # This function is the evaluation function, we want
 # to give high score to more zero'ed chromosomes
-def eval_func(chromosome):
+def eval_func(genome):
    score = 0.0
-
    # iterate over the chromosome
-   for value in chromosome:
-      if value==0:
-         score += 0.1
+   for value in genome:
+      if value==0: score += 0.1
    return score
 
-# Enable the logging system
-pyevolve.logEnable()
+def run_main():
+   # Genome instance
+   genome = G1DList.G1DList(200)
+   genome.setParams(rangemin=0, rangemax=10)
 
-# Genome instance
-genome = G1DList.G1DList(200)
-genome.setParams(rangemin=0, rangemax=10)
+   # The evaluator function (objective function)
+   genome.evaluator.set(eval_func)
 
-# The evaluator function (objective function)
-genome.evaluator.set(eval_func)
+   # Genetic Algorithm Instance
+   ga = GSimpleGA.GSimpleGA(genome)
+   ga.selector.set(Selectors.GRouletteWheel)
+   ga.setGenerations(800)
+   ga.stepCallback.set(evolve_callback)
 
-# Genetic Algorithm Instance
-ga = GSimpleGA.GSimpleGA(genome)
-ga.selector.set(Selectors.GRouletteWheel)
-ga.setGenerations(800)
-ga.stepCallback.set(evolve_callback)
+   # Do the evolution
+   ga.evolve()
 
-# Do the evolution, with stats dump
-# frequency of 10 generations
-ga.evolve()
+   # Best individual
+   print ga.bestIndividual()
 
-# Best individual
-print ga.bestIndividual()
+if __name__ == "__main__":
+   run_main()
+
