59db42d4a9e95048d94e7862d05f594a813cefae
tarantulae <tarantulae@07ed1b73-6f11-4a40-98be-48c2d1321411>
2009-12-02 00:07:21 +0000

    Fixed the code for the Ex9
    git-svn-id: https://pyevolve.svn.sourceforge.net/svnroot/pyevolve@275 07ed1b73-6f11-4a40-98be-48c2d1321411

--- pyevolve_ex9_g2dlist.py
+++ pyevolve_ex9_g2dlist.py
@@ -12,28 +12,32 @@ def eval_func(chromosome):
    # iterate over the chromosome
    for i in xrange(chromosome.getHeight()):
       for j in xrange(chromosome.getWidth()):
-         # You can use the chromosome.getItem(i, j)
+         # You can use the chromosome.getItem(i, j) too
          if chromosome[i][j]==0:
             score += 0.1
    return score
 
-# Genome instance
-genome = G2DList.G2DList(8, 5)
-genome.setParams(rangemin=0, rangemax=10)
+def run_main():
+   # Genome instance
+   genome = G2DList.G2DList(8, 5)
+   genome.setParams(rangemin=0, rangemax=100)
 
-# The evaluator function (objective function)
-genome.evaluator.set(eval_func)
-genome.crossover.set(Crossovers.G2DListCrossoverSingleHPoint)
-genome.mutator.set(Mutators.G2DListMutatorIntegerGaussian)
+   # The evaluator function (objective function)
+   genome.evaluator.set(eval_func)
+   genome.crossover.set(Crossovers.G2DListCrossoverSingleHPoint)
+   genome.mutator.set(Mutators.G2DListMutatorIntegerRange)
 
-# Genetic Algorithm Instance
-ga = GSimpleGA.GSimpleGA(genome)
-ga.selector.set(Selectors.GRouletteWheel)
-ga.setGenerations(1200)
+   # Genetic Algorithm Instance
+   ga = GSimpleGA.GSimpleGA(genome)
+   ga.setGenerations(800)
 
-# Do the evolution, with stats dump
-# frequency of 10 generations
-ga.evolve(freq_stats=100)
+   # Do the evolution, with stats dump
+   # frequency of 10 generations
+   ga.evolve(freq_stats=100)
 
-# Best individual
-print ga.bestIndividual()
+   # Best individual
+   print ga.bestIndividual()
+
+
+if __name__ == "__main__":
+   run_main()
